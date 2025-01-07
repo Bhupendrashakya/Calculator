d62ff62e080b5f78e9925b5f9bda85aed87aefd1
Bhupendra shakya <bhupendrashakya714@gmail.com>
2025-01-07 13:18:29 +0530

    main function added

--- Calculator.java
+++ Calculator.java
@@ -11,6 +11,14 @@ return a*b;
 public static int divide(int a , int b){
 return a/b; 
 }
-
+public static void main(String[] args){
+int a = 4; 
+int b = 6 ; 
+Calculator cal=new Calculator();
+cal.add(a,b); 
+cal.substract (a,b); 
+cal.multiply(a,b);
+cal.divide(a,b);
+}
 
 }
